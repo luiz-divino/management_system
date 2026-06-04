ae11c4d2616095178731dd051a19cd66cb1ee52c
luiz-divino <nandodivinodev@gmail.com>
2026-06-04 16:26:30 -0300

    feat: building endpoints for create e delete tasks

--- src/routes.ts
+++ src/routes.ts
@@ -1,25 +1,43 @@
 import { Router } from "express";
-import { validateUserSchema } from "./middlewares/validateUserSchema.js";
+import { validateSchema } from "./middlewares/validateSchema.js";
 import { LoginUserSchema, createUserSchema } from "./schemas/userSchema.js";
 import { CreateUserController } from "./controllers/user/CreateUserController.js";
 import { authUserController } from "./controllers/user/AuthUserController.js";
+import { createTaskSchema, deleteTaskSchema } from "./schemas/taskSchema.js";
+import { createTaskController } from "./controllers/task/CreateTaskController.js";
+import { isAutenticated } from "./middlewares/isAutenticated.js";
+import { DeleteTaskController } from "./controllers/task/DeleteTaskController.js";
 
 const router = Router();
 
-router.get('/hello', (_, res)=>{
-  res.send('Hello World!')
-})
+router.get("/hello", (_, res) => {
+    res.send("Hello World!");
+});
 
 router.post(
-  "/users",
-  validateUserSchema(createUserSchema),
-  new CreateUserController().handle,
+    "/users",
+    validateSchema(createUserSchema),
+    new CreateUserController().handle,
 );
 
 router.post(
-  "/session",
-  validateUserSchema(LoginUserSchema),
-  authUserController.handle,
+    "/session",
+    validateSchema(LoginUserSchema),
+    authUserController.handle,
+);
+
+router.post(
+    "/tasks",
+    validateSchema(createTaskSchema),
+    isAutenticated,
+    createTaskController.handle,
+);
+
+router.delete(
+    "/tasks/:taskId",
+    validateSchema(deleteTaskSchema),
+    isAutenticated,
+    new DeleteTaskController().handle,
 );
 
 export { router };
