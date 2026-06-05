f365eb881f547c3f48a8ef84518ccf2efe895d2c
luiz-divino <nandodivinodev@gmail.com>
2026-06-05 00:51:25 -0300

    chore: add middleware to errors

--- src/server.ts
+++ src/server.ts
@@ -2,19 +2,21 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import { router } from "./routes.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
 
 const app = express();
 app.use(express.json());
 app.use(
-  cors({
-    origin: "*", // Durante o desenvolvimento você pode deixar *, depois mude para a URL da Vercel
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  }),
+    cors({
+        origin: "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    }),
 );
 app.use(router);
+app.use(errorHandler);
 
 const port = Number(process.env.PORT) || 3333;
 app.listen(port, "0.0.0.0", () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
