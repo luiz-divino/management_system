f8853bfd011ed3befc95861636a6f6c2f7fa8e43
luiz-divino <nandodivinodev@gmail.com>
2026-06-06 00:25:50 -0300

    feat: add method patch in the cors

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(
     cors({
         origin: "*",
-        methods: ["GET", "POST", "PUT", "DELETE"],
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
     }),
 );
