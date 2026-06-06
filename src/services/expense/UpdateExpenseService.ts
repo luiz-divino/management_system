279eb61423ea5547ec0c0a7b24cbea7a676f81fe
luiz-divino <nandodivinodev@gmail.com>
2026-06-06 00:35:46 -0300

    feat: implement update expense service to update an existing expense in the database

---
+++ src/services/expense/UpdateExpenseService.ts
@@ -0,0 +1,78 @@
+import { AppError } from "../../errors/AppError.js";
+import prismaClient from "../../prisma/index.js";
+
+interface UpdateExpenseData {
+    expenseId: string;
+    id_user: string;
+    description?: string;
+    amount?: number;
+    date?: string;
+    category?: string;
+}
+
+export class UpdateExpenseService {
+    async execute({
+        expenseId,
+        description,
+        amount,
+        date,
+        category,
+        id_user,
+    }: UpdateExpenseData) {
+        const expense = await prismaClient.expense.findFirst({
+            where: {
+                id: expenseId,
+                id_user,
+            },
+            select: {
+                id: true,
+            },
+        });
+        if (!expense) {
+            throw new AppError("Expense not found", 404);
+        }
+
+        const data: {
+            description?: string;
+            amount?: number;
+            date?: Date;
+            category?: string;
+        } = {};
+
+        if (description !== undefined) {
+            data.description = description;
+        }
+
+        if (amount !== undefined) {
+            data.amount = amount;
+        }
+        if (date !== undefined) {
+            const parsedDate = new Date(date);
+
+            if (Number.isNaN(parsedDate.getTime())) {
+                throw new AppError("Date is invalid", 400);
+            }
+
+            data.date = parsedDate;
+        }
+
+        if (category !== undefined) {
+            data.category = category;
+        }
+
+        const updatedExpense = await prismaClient.expense.update({
+            where: {
+                id: expenseId,
+            },
+            data,
+            select: {
+                id: true,
+                description: true,
+                amount: true,
+                date: true,
+                category: true,
+            },
+        });
+        return updatedExpense;
+    }
+}
