3132310ca77318e0f25f3fc0188e7ceb8664a3f7
luiz-divino <nandodivinodev@gmail.com>
2026-07-02 15:16:05 -0300

    chore: change the way to get user id to show infomation

--- src/controllers/user/DetailsUserController.ts
+++ src/controllers/user/DetailsUserController.ts
@@ -2,7 +2,7 @@ import { datailsUser } from "../../services/user/DetailsUserService.js";
 import { Request, Response } from "express";
 export class DetailsUserController {
     async handle(req: Request, res: Response) {
-        const { user_id } = req.body;
+        const user_id = req.userId;
         const user = await datailsUser.execute(user_id as string);
         return res.json(user);
     }
